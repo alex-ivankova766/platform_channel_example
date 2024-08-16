9b1cf1d40b23e2e9b48726bc62920eecf0f156d0
ivankova.alexandra766 <ivankova.alexandra766@gmail.com>
2024-08-16 15:19:00 +0300

    library connect

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,12 +4,23 @@ import UIKit
 @main
 @objc class AppDelegate: FlutterAppDelegate {
     private let channelName = "com.example/platform_channel/getNativeData"
+    private let CHANNEL = "com.example.datetime_example/dateTime"
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     let controller = window?.rootViewController as! FlutterViewController
     let methodChannel = FlutterMethodChannel(name: channelName, binaryMessenger: controller.binaryMessenger)
+    let library_channel = FlutterMethodChannel(name: CHANNEL, binaryMessenger: controller.binaryMessenger)
+
+    library_channel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
+      if call.method == "getCurrentDateTime" {
+          let currentDateTime = Date().description
+          result(currentDateTime)
+      } else {
+          result(FlutterMethodNotImplemented)
+      }
+    }
 
     methodChannel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
       if call.method == "getNativeMessage" {
--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,23 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   static const platform = MethodChannel('com.example/platform_channel/getNativeData');
 
+  static const platform_library = MethodChannel('com.example.datetime_example/dateTime');
+  String _dateTime = 'Unknown';
+
   String _response = 'Waiting for response...';
+
+  Future<void> _getCurrentDateTime() async {
+    try {
+      final String result = await platform_library.invokeMethod('getCurrentDateTime');
+      setState(() {
+        _dateTime = result;
+      });
+    } on PlatformException catch (e) {
+      setState(() {
+        _dateTime = "Failed to get dateTime: '${e.message}'.";
+      });
+    }
+  }
 
   Future<void> _getNativeMessage() async {
     String response;
@@ -60,6 +76,16 @@ class _MyHomePageState extends State<MyHomePage> {
             ElevatedButton(
               onPressed: _getNativeMessage,
               child: const Text('Get Native Message'),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              _dateTime,
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _getCurrentDateTime,
+              child: const Text('Get Native DateTime'),
             ),
           ],
         ),
